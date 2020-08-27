29056e19e754ccf7858cd79ee558bf971ab59e94
karemarsy <karemarsy@gmail.com>
2020-08-27 13:06:47 +0200

    Using radium

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Radium from 'radium'
 
 const person = (props) =>{
+  const style = {
+    '@media (min-width: 500px)':{
+    width:'450px'
+    }
+  };
   return (
-  <div>
-    <h5>I'm {props.name} and I'm {props.age} years old</h5>
+  <div style={style}>
+    <h5 onClick= {props.Click}>I'm {props.name} and I'm {props.age} years old</h5>
   <p>{props.children}</p>
   </div>
   )
 };
 
-export default person;
+export default Radium(person);
