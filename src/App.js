6a480da51f13947d896f7d924088d7708a3e5a5a
karemarsy <karemarsy@gmail.com>
2020-10-01 09:21:01 +0200

    Finish the burger builder

--- src/App.js
+++ src/App.js
@@ -1,75 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
-import Radium,{ StyleRoot} from 'radium';
+
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  state = {
-    person: [
-      {id:1, name:'arsene',age:'26'},
-      {id:2,name:'herve', age:'34'},
-      {id:3,name:'lewis', age:'34'},
-    ],
-    personShow: false
-  }
-
-   deletePersonHandler =(personIndex) =>{
-     const person = this.state.person
-     person.splice(personIndex, 1);
-     this.setState({persons: Person})
-
-   }
-
-  togglePersonsHandler = () =>{
-   const doesShow = this.state.personShow;
-   this.setState ({personShow : !doesShow});
-  }
-
-  nameChangeHandler = (event) =>{
-    this.setState ({
-      person:[
-        {name: event.target.value,age:'25'},
-        {name:'herve', age:'24'},
-        {name:'lewis', age:'34'},
-      ]
-    })
-  }
-  render(){
-
-    let persons = null;
-    if (this.state.personShow){
-      persons = (
-        <div>
-          {this.state.person.map ((arsene, index) => {
-            return (<Person Click={ ()=>this.deletePersonHandler(index)} name={arsene.name} age={arsene.age} key={arsene.id}/>)
-          })}
-        </div> 
-      );
-
-    }
-
-    const classes = [];
-    if(this.state.person.length <=2 ){
-      classes.push('red');
-    }
-    if(this.state.person.length<= 1){
-      classes.push('bold','red');
-    }
+  render () {
     return (
-      <StyleRoot>
-      <div className="App">
-
-<h1>hi everyone</h1>
-<p className={classes.join (' ')}>change colors</p>       
-<button onClick={this.togglePersonsHandler}>click here </button>
-        
-        {persons}
-        
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
-      </StyleRoot>
     );
   }
 }
 
-export default Radium(App);
-
+export default App;
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux2 from '../../../hoc/Aux2/Aux2';
+
+const sideDrawer = ( props ) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+    return (
+        <Aux2>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux2>
+    );
+};
+
+export default sideDrawer;
